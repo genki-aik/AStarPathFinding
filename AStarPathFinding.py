219b5c4cd20fcdd51ce0e9e0fa78b1ba52461fdf
Genki Aikawa <genki@MacBook-Pro.local>
2020-11-27 01:27:26 -0500

    Draws the start and end node as well as barrier

--- AStarPathFinding.py
+++ AStarPathFinding.py
@@ -51,6 +51,9 @@ class Node:
 
     def reset(self):
         self.color == WHITE
+
+    def make_start(self):
+        self.color = ORANGE
     
     def make_closed(self):
         self.color = RED
@@ -91,7 +94,7 @@ def make_grid(rows, width):
         grid.append([])
         for j in range(rows):
             node = Node(i, j, gap, rows)
-            grid[i].append(spot)
+            grid[i].append(node)
 
     return grid
 
@@ -107,8 +110,55 @@ def draw(win, grid, rows, width):
 
     for row in grid:
         for node in row:
-            spot.draw(win)
+            node.draw(win)
 
     draw_grid(win, rows, width)
     pygame.display.update()
 
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
+def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+
+    run = True
+    started = False
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if started:
+                continue
+
+            if pygame.mouse.get_pressed()[0]: # LEFT MOUSE BUTTOn
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                if not start:
+                    start = node
+                    start.make_start()
+
+                elif not end:
+                    end = node
+                    end.make_end()
+
+                elif node != end and node != start:
+                    node.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]: # RIGHT MOUSE BUTTON
+                pass
+    pygame.quit()
+
+main(WIN, WIDTH)
